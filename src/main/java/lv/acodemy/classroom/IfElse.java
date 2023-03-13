9cd07d72331f906e3441e9b37feb2affac6bae10
ijakovlev10 <akovlevivan532@gmail.com>
2023-03-13 09:25:46 -0700

    [classroom_01032023]: Add classroom progress

---
+++ src/main/java/lv/acodemy/classroom/IfElse.java
@@ -0,0 +1,36 @@
+package lv.acodemy.classroom;
+
+public class IfElse {
+    public static void main(String[] args) {
+//    boolean x = 5 > 2;
+//        System.out.println(x);
+//
+//        if(x) {
+//            System.out.println("Yes, 5 is more than 2");
+
+//        int number = 21;
+//        if(number % 2 ==0) {
+//            System.out.println("Number: " + number + "is even number");
+//            System.out.println("number: " + number + "in NOT even number");
+//        }
+//
+//        int age = 17;
+//        if (age <= 17) {
+//            System.out.println("You are not allowed to buy here!");
+//        } else {
+//            System.out.println("Welcome to our shop!");
+//            boolean isSummer = true;
+//            if(isSummer) {
+//                System.out.println("Summer months: June, July, August");
+//            }
+
+            int age = 17;
+            if (age <=17) {
+                System.out.println("You are allowed to buy here!");
+            } else if (age <0) {
+                System.out.println("Who are you?");
+                } else {
+                System.out.println("Welcome to our shop!");
+        }
+    }
+}
